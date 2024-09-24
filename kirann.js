1bf2be7c6e2b529ec7c007cd49b59357acbd7fa7
Kiran Kumar <kirankumarpilli82gmail.com>
2024-09-24 19:57:41 +0530

    hii

---
+++ kirann.js
@@ -0,0 +1,21 @@
+// addition
+
+function hello(a,b){
+    return a+b
+}
+hello(3,5);
+console.log(hello(3,5))
+// subtraction
+
+function helloo(a,b){
+    return a-b
+}
+
+console.log(helloo(3,5))
+
+// multiplication
+function hii(a,b){
+    return a*b
+}
+
+console.log(hii(3,5))
